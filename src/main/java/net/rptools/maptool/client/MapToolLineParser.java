bcacfe8cff30dfb997153a50ba7b4f4abd243e62
Oliver Szymanski <oliver.szymanski@source-knights.com>
2019-03-12 19:51:54 +0000

    Add macro.catchAssert

--- src/main/java/net/rptools/maptool/client/MapToolLineParser.java
+++ src/main/java/net/rptools/maptool/client/MapToolLineParser.java
@@ -1465,7 +1465,6 @@ public class MapToolLineParser {
       boolean catchAssert = BigDecimal.ONE.equals(resolver.getVariable("macro.catchAssert"));
       if (!catchAssert) throw e;
       MapTool.addLocalMessage(e.getMessage());
-
       // return an empty result to not collide with tooltips
       // when catching an assert`
       Result result = new Result("");
