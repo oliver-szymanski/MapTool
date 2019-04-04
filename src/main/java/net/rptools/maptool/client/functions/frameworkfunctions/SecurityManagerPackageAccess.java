0d3a1455c4ba2c697e45a4c844ff64081670111d
naciron <oliver.szymanski@source-knights.com>
2019-04-04 23:48:03 +0100

    Add permission to allow the js.eval build in functions

--- src/main/java/net/rptools/maptool/client/functions/frameworkfunctions/SecurityManagerPackageAccess.java
+++ src/main/java/net/rptools/maptool/client/functions/frameworkfunctions/SecurityManagerPackageAccess.java
@@ -16,32 +16,32 @@ package net.rptools.maptool.client.functions.frameworkfunctions;
 
 class SecurityManagerPackageAccess extends SecurityManager {
 
-  // checkPackageAccess needs to be overridden
-  @Override
-  public void checkPackageAccess(String pkg) {
-    try {
-      // super will not check the AccessControlContext for permission
-      // so needs to be overridden and more checks added.
-      // otherwise too much is granted (it only checks based on java modules)
-      super.checkPackageAccess(pkg);
+	// checkPackageAccess needs to be overridden
+	@Override
+	public void checkPackageAccess(String pkg) {
+		try {
+			// super will not check the AccessControlContext for permission
+			// so needs to be overridden and more checks added.
+			// otherwise too much is granted (it only checks based on java modules)
+			super.checkPackageAccess(pkg);
 
-      // restrict access to MapTool packages, except some.
-      // core will be allowed as it has all permissions anyway.
-      // in case class access is required check the other permission methods
-      // to override.
-      if (pkg.startsWith("net.rptools.")
-          && !pkg.equals("net.rptools.maptool.client")
-          && !pkg.equals("net.rptools.maptool.client.ui.zone")
-          && !pkg.equals("net.rptools.maptool.client.ui.commandpanel")
-          && !pkg.equals("net.rptools.maptool.client.functions")
-          && !pkg.equals("net.rptools.maptool.client.functions.frameworkfunctions")
-          && !pkg.equals("net.rptools.maptool.model")) {
-        checkPermission(new RuntimePermission("accessClassInPackage." + pkg));
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      // MapTool.addLocalMessage("Permission error: " + e.getMessage());
-      throw e;
-    }
-  }
+			// restrict access to MapTool packages, except some.
+			// core will be allowed as it has all permissions anyway.
+			// in case class access is required check the other permission methods
+			// to override.
+			if (pkg.startsWith("net.rptools.") && !pkg.equals("net.rptools.maptool.client")
+			    && !pkg.equals("net.rptools.maptool.client.ui.zone")
+			    && !pkg.equals("net.rptools.maptool.client.ui.commandpanel")
+			    && !pkg.equals("net.rptools.maptool.client.functions")
+			    && !pkg.equals("net.rptools.maptool.client.functions.frameworkfunctions")
+			    && !pkg.equals("net.rptools.maptool.client.script.javascript.api")
+			    && !pkg.equals("net.rptools.maptool.model")) {
+				checkPermission(new RuntimePermission("accessClassInPackage." + pkg));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			// MapTool.addLocalMessage("Permission error: " + e.getMessage());
+			throw e;
+		}
+	}
 }
